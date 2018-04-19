f1459dd727cde954c1c2af539400e9e6aa1715da
Andrea Roizman <amroizman@uc.cl>
2018-04-19 16:45:44 -0300

    edited dirvers view

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Dashboard from './containers/Dashboard/Dashboard';
 class App extends Component {
   render() {
     return (
-      
-        <div>  
+
+        <div>
           <Layout>
             <Switch>
               <Route path="/users" component={ Users } />
--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,14 +1,86 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { ListGroup, ListGroupItem, Button, Row, Col, Container } from 'reactstrap';
+import Aux from '../../hoc/Aux';
+
+
+
 
 
 class Users extends React.Component {
 
 
+    onDeleteBtnClick(){
+
+    }
+
     render(){
 
-        return (
+      const pStyle = {
+        justifyContent: "flex-end"
+      };
 
-            <h1> Users </h1>
+      const divStyle = {
+        display: "flex"
+
+      }
+
+        return (
+            <Aux>
+            <h1> Drivers </h1>
+            <Container>
+              <ListGroup>
+                <ListGroupItem style={divStyle} className="justify-content-between">
+                  <Link to="/">Florencia Barrios</Link>
+                  <div style={pStyle}>
+                    <Link style={pStyle} className="btn btn-secondary btn-sm" to="/">Edit</Link>{' '}
+                    <Button style={pStyle} color="danger" size="sm" onClick={() => this.onDeleteBtnClick()}>Delete</Button>
+                  </div>
+                </ListGroupItem>
+
+                <ListGroupItem style={divStyle} className="justify-content-between">
+                  <Link to="/">Polo Vargas</Link>
+                  <div style={pStyle}>
+                    <Link style={pStyle} className="btn btn-secondary btn-sm" to="/">Edit</Link>{' '}
+                    <Button style={pStyle} color="danger" size="sm" onClick={() => this.onDeleteBtnClick()}>Delete</Button>
+                  </div>
+                </ListGroupItem>
+
+                <ListGroupItem style={divStyle} className="justify-content-between">
+                  <Link to="/">Benjamin Peña</Link>
+                  <div style={pStyle}>
+                    <Link style={pStyle} className="btn btn-secondary btn-sm" to="/">Edit</Link>{' '}
+                    <Button style={pStyle} color="danger" size="sm" onClick={() => this.onDeleteBtnClick()}>Delete</Button>
+                  </div>
+                </ListGroupItem>
+
+                <ListGroupItem style={divStyle} className="justify-content-between">
+                  <Link to="/">Andrea Roizman</Link>
+                  <div style={pStyle}>
+                    <Link style={pStyle} className="btn btn-secondary btn-sm" to="/">Edit</Link>{' '}
+                    <Button style={pStyle} color="danger" size="sm" onClick={() => this.onDeleteBtnClick()}>Delete</Button>
+                  </div>
+                </ListGroupItem>
+
+                <ListGroupItem style={divStyle} className="justify-content-between">
+                  <Link to="/">Vicente Claro</Link>
+                  <div style={pStyle}>
+                    <Link style={pStyle} className="btn btn-secondary btn-sm" to="/">Edit</Link>{' '}
+                    <Button style={pStyle} color="danger" size="sm" onClick={() => this.onDeleteBtnClick()}>Delete</Button>
+                  </div>
+                </ListGroupItem>
+
+                <ListGroupItem style={divStyle} className="justify-content-between">
+                  <Link to="/">Felipe Garrido</Link>
+                  <div style={pStyle}>
+                    <Link style={pStyle} className="btn btn-secondary btn-sm" to="/">Edit</Link>{' '}
+                    <Button style={pStyle} color="danger" size="sm" onClick={() => this.onDeleteBtnClick()}>Delete</Button>
+                  </div>
+                </ListGroupItem>
+
+              </ListGroup>
+            </Container>
+            </Aux>
 
         );
     }
