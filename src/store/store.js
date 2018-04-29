35125342467ad1ecda11ad71f7039d0413eaa326
lsvargas <lsvargas@uc.cl>
2018-04-29 01:22:21 -0300

    persisted state fix

--- src/store/store.js
+++ src/store/store.js
@@ -11,16 +11,16 @@ const rootReducer = combineReducers({
 });
 
 
-function saveToLocalStorage(state) {
+const saveToLocalStorage = (state) => {
   try {
     const serializedState = JSON.stringify(state);
     localStorage.setItem('state', serializedState);
   } catch (e) {
     console.log(e);
   }
-}
+};
 
-function loadFromLocalStorage() {
+const loadFromLocalStorage = () => {
   try {
     const serializedState = localStorage.getItem('state');
     if (serializedState === null) return undefined;
@@ -29,13 +29,14 @@ function loadFromLocalStorage() {
     console.log(e);
     return undefined;
   }
-}
+};
 
 const persistedState = loadFromLocalStorage();
 
 const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(thunk)));
 
-store.subscribe(() => saveToLocalStorage(store.getState()));
+
+store.subscribe(() => saveToLocalStorage({ auth: store.getState().auth }));
 
 
 export default store;
