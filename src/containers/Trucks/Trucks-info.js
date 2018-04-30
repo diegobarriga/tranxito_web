aa0188cf77578e5285417799b3ac0675557f2db6
Florencia Barrios <fbarrios1@uc.cl>
2018-04-30 01:25:40 -0300

    Added searcbox an sorted results for vehicles

--- src/containers/Trucks/Trucks-info.js
+++ src/containers/Trucks/Trucks-info.js
@@ -5,10 +5,18 @@ import Loader from '../../components/Loader/Loader';
 import * as actions from '../../store/actions/vehicles';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Input } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import styles from '../../assets/styles/forms.css';
 
 class TrucksInfo extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      search: ''
+    };
+    this.updateSearch = this.updateSearch.bind(this);
   }
 
   componentDidMount() {
@@ -16,16 +24,38 @@ class TrucksInfo extends React.Component {
 
   }
 
+  updateSearch(event) {
+    this.setState({ search: event.target.value })
+  }
 
 
   render() {
+
+    let filtered_vehicles = this.props.vehicles.filter(
+      (vehicle) => {
+        return (vehicle.vin.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 ||
+        vehicle.model.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 ||
+        vehicle.car_maker.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 ||
+        vehicle.plaque.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1);
+      }
+    );
     return (
       <div>
+
+        <div className="inlineBox">
+          <FontAwesomeIcon icon="search" className="customIcon" /><input className="customInput" type='text' value={this.state.search} onChange={this.updateSearch}></input>
+          <div className="buttons">
+            <Link className="btn btn-sm green spacing" to="/vehicles/new_vehicle"><FontAwesomeIcon icon="car" color="white" /> Create vehicle</Link>
+            <Link className="btn btn-sm green" to="/vehicles/new_vehicles"><FontAwesomeIcon icon="car" color="white" /><FontAwesomeIcon icon="car" color="white" /> Create multiple vehicles</Link>
+          </div>
+        </div>
+
+
         {this.props.loading && <Loader />}
-        {console.log(this.props.vehicles)}
+        {console.log(filtered_vehicles)}
         <ListGroup>
           {
-              this.props.vehicles.map(truck => (<TruckRow
+              filtered_vehicles.sort(function(a, b){return a.car_maker > b.car_maker}).map(truck => (<TruckRow
                 key={truck.id}
                 vin={truck.vin}
                 CMV_power_unit_number={truck.CMV_power_unit_number}
@@ -42,10 +72,6 @@ class TrucksInfo extends React.Component {
     );
   }
 }
-
-
-
-
 
 const mapStateToProps = state => {
     return {
