fe324a85dad27f2206982739f6537fe1f893d46b
Federico Kunze <fekunze@berkeley.edu>
2018-05-01 17:28:49 -0300

    Added edit forms

---
+++ src/containers/MotorCarriers/EditMotorCarrierForm.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import validator from 'validator';
+import { Button, Form, Input } from 'semantic-ui-react';
+var _ = require('lodash');
+
+
+class EditMotorCarrierForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+      data: {
+        name: this.props.data.name,
+        USDOT_number: this.props.data.USDOT_number,
+        multiday_basis_used: this.props.data.multiday_basis_used
+      },
+      isLoading: false,
+      redirectTo: false
+    }
+    this.isValidEdit = this.isValidEdit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.validateInput = this.validateInput.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({
+      ...this.state,
+      data: { ...this.state.data, [event.target.name]: event.target.value }
+    });
+  }
+
+  validateInput(data) {
+    let errors = {};
+    if (_.isEmpty(String(data.name))) {
+      errors.name = 'This field is required';
+    }
+    else if (String(data.name).length > 120 || String(data.name).length < 4) {
+      errors.name = 'Name must be between 4-120 characters long';
+    }
+    if (_.isEmpty(String(data.USDOT_number))) {
+      errors.USDOT_number = 'This field is required';
+    }
+    else if (!validator.isInt(String(this.USDOT_number), {min: 0, max: 999999999})) {
+      errors.USDOT_number = 'USDOT number must be between 0-999,999,999';
+    }
+    if (_.isEmpty(String(data.multiday_basis_used))) {
+      errors.multiday_basis_used = 'This field is required';
+    }
+    else if (!validator.isInt(String(this.multiday_basis_used), {min: 7, max: 8})) {
+      errors.multiday_basis_used = 'Multiday basis used number must be 7 or 8';
+    }
+    return {
+      errors,
+      isValid: _.isEmpty(errors)
+    }
+  }
+
+  submitHandler(event){
+    event.preventDefault(); // prevents reload of the page
+    if (this.isValidEdit()) {
+      this.setState({ errors: {}, isLoading: true});
+      this.props.submit(this.state.data);
+      // .catch(
+      //   (err) => this.setState({ errors: err.response.data.errors, isLoading: false })
+      // );
+    }
+  }
+
+  isValidEdit(){
+    const { errors, isValid } = this.validateInput(this.state.data);
+    if (!isValid) this.setState({ errors });
+    return isValid;
+  }
+
+  render() {
+    const { errors, isLoading } = this.state;
+    return (
+        <Form onSubmit={this.submitHandler}>
+          <Form.Group>
+            <Form.Input
+              type="text"
+              name="name"
+              onChange={this.onChange}
+              placeholder="Name"
+              error={errors.name}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input
+              type="number"
+              name="USDOT_number"
+              onChange={this.onChange}
+              placeholder="USDOT Number"
+              error={errors.USDOT_number}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Input
+            type="text"
+            name="multiday_basis_used"
+            onChange={this.onChange}
+            placeholder="Multiday basis used"
+            error={errors.multiday_basis_used}
+            />
+          </Form.Group>
+          <Button type="submit" loading={isLoading}>Update</Button>
+        </Form>
+    );
+  }
+}
+
+EditMotorCarrierForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
+
+export default EditMotorCarrierForm;
