76e257d75c5b89905e0861cd1e8b2211cc99c381
lsvargas <lsvargas@uc.cl>
2018-05-08 00:31:03 -0300

    case-sensitive

---
+++ src/store/actions/mCarrier.js
@@ -0,0 +1,47 @@
+import * as actionTypes from './actionTypes';
+import api from '../../services/api';
+
+export const setMotorCarriers = motorcarriers => ({
+  type: actionTypes.SET_MOTORCARRIER,
+  motorCarriers: motorcarriers,
+});
+
+export const createMCarrierStart = () => ({
+  type: actionTypes.START_MCCREATE,
+});
+
+export const createMCSuccess = regData => ({
+  regData,
+  type: actionTypes.CREATEMC_SUCCESS,
+});
+
+
+export const initMCarriers = token => (dispatch) => {
+  api.motorCarriers.getMotorCarriers(token)
+    .then((response) => {
+      console.log(response.data);
+      dispatch(setMotorCarriers(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+
+export const carrierRegister = (name, usdotNumber, multidayBasisUsed, token) => (dispatch) => {
+  dispatch(createMCarrierStart());
+  const regData = {
+    name,
+    USDOT_number: usdotNumber,
+    multiday_basis_used: multidayBasisUsed,
+  };
+
+  api.motorCarriers.createMotorCarrier(regData, token)
+    .then((response) => {
+      console.log(response.data);
+      dispatch(createMCSuccess(regData));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
