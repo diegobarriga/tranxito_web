a0437125758bb581a9a48b430c56435665bd28ce
lsvargas <lsvargas@uc.cl>
2018-05-17 16:40:29 -0400

    map-fix

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,29 +5,6 @@ import { Link } from 'react-router-dom';
 import '../../assets/styles/sidebar.css';
 import Aux from '../../hoc/Aux';
 
-<<<<<<< Updated upstream
-const sidebar = props => (
-
-  <div className="sidebar">
-    <div>
-      <ListGroup>
-        {props.isAdm ?
-          <Link className="list-group-item-action list-group-item" to="/motor_carriers">Motor Carriers</Link>
-          :
-          <Aux>
-            <Link className="list-group-item-action list-group-item disabledCursor" onClick={e => e.preventDefault()} to="/dashboard">Dashboard</Link>
-            <Link className="list-group-item-action list-group-item" to="/drivers">Drivers</Link>
-            <Link className="list-group-item-action list-group-item" to="/vehicles">Vehicles</Link>
-            <Link className="list-group-item-action list-group-item disabledCursor" onClick={e => e.preventDefault()} to="/logs">Alerts</Link>
-          </Aux> }
-
-      </ListGroup>
-    </div>
-
-  </div>
-);
-=======
-
 class sidebar extends Component {
   state = {
     dashboardClicked: false,
