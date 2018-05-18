3daa393d01ff8f2fb893f0bc3245e761c4c564b3
Andrea Roizman <amroizman@uc.cl>
2018-05-18 17:20:22 -0400

    Error handling

--- src/containers/Users/graph.js
+++ src/containers/Users/graph.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 import { EVENT_CODES } from '../../utils/eventTypes';
-// import Loader from '../../components/Loader/Loader';
+import Loader from '../../components/Loader/Loader';
 
 class Graph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      actual: 1,
+      loading: false,
       chartData: {
         xLabels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
           13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
@@ -26,86 +26,67 @@ class Graph extends React.Component {
           },
         ],
       },
-      logs: [
-        {
-          event_type: 1,
-          event_code: 4,
-          event_timestamp: '2018-04-21T02:10:20.660Z',
-        },
-        [{
-          event_type: 1,
-          event_code: 1,
-          event_timestamp: '2018-04-21T03:04:20.660Z',
-        },
-        {
-          event_type: 1,
-          event_code: 3,
-          event_timestamp: '2018-04-21T07:37:20.660Z',
-        },
-        {
-          event_type: 1,
-          event_code: 2,
-          event_timestamp: '2018-04-21T11:18:20.660Z',
-        },
-        {
-          event_type: 1,
-          event_code: 4,
-          event_timestamp: '2018-04-21T13:42:20.660Z',
-        },
-        {
-          event_type: 1,
-          event_code: 2,
-          event_timestamp: '2018-04-21T23:21:20.660Z',
-        },
-        ],
-      ],
       api_logs: null,
-
     };
+    this.getData = this.getData.bind(this);
+    this.processData = this.processData.bind(this);
   }
 
   componentDidMount() {
-    this.state.api_logs = api.people.getUserDutyStatusChange(this.props.id, this.props.token);
+    this.getData();
+  }
+
+  getData() {
+    this.setState({ loading: true });
+    api.people.getUserDutyStatusChange(this.props.id, this.props.token)
+      .then((response) => {
+        const logs = response.data;
+        this.setState({ loading: false, api_logs: logs }, this.processData);
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+        console.log(error.message);
+      });
+  }
+
+  processData = () => {
+    const graphData = this.state.chartData;
+    graphData.datasets[0].data = [];
+
+    // Agregar el estado inicial
+    graphData.datasets[0].data.push({
+      x: 0,
+      y: EVENT_CODES[1][this.state.api_logs[0].event_code],
+    });
+    this.state.api_logs[1].map((event) => {
+      graphData.datasets[0].data.push({
+        x: parseFloat(event.event_timestamp.substring(11, 13)) +
+           (parseFloat(event.event_timestamp.substring(14, 16)) / 60),
+        y: EVENT_CODES[1][event.event_code],
+      });
+    });
+    this.setState({ chartData: graphData });
   }
 
   render() {
-    // if (this.state.api_logs === null) return <Loader />;
-
-    this.state.chartData.datasets[0].data = [];
-    this.state.actual = this.state.logs[0];
-
-    this.state.chartData.xLabels.map((hour) => {
-      this.state.logs[1].map((event) => {
-        if (hour === parseFloat(event.event_timestamp.substring(11, 13))) {
-          if (this.state.actual.event_code !== event.event_code) {
-            this.state.actual = event;
-          }
-        }
-      });
-      this.state.chartData.datasets[0].data.push({
-        x: parseFloat(this.state.actual.event_timestamp.substring(11, 13)) +
-         (parseFloat(this.state.actual.event_timestamp.substring(14, 16)) / 60),
-        y: EVENT_CODES[1][this.state.actual.event_code],
-      });
-    });
+    if (this.state.loading === true) return <Loader />;
 
     return (
       <Scatter
         data={this.state.chartData}
         height={75}
         options={{
-
               title: {
                 display: true,
                 text: 'Last 24 hours',
                 fontSize: 25,
-
               },
               legend: {
                 display: false,
               },
               scales: {
                         xAxes: [{
+                          type: 'linear',
                           display: true,
                           scaleLabel: {
                             display: true,
--- src/services/api.js
+++ src/services/api.js
@@ -44,7 +44,8 @@ export default {
     getUserMotorCarrier: userId =>
       axios.get(`${apiPath}/People/${userId}/motorCarrier`),
     getUserDutyStatusChange: (userId, token) =>
-      axios.get(`${apiPath}/People/${userId}/dutyStatusChange?access_token=${token}`),
+      axios.get(`https://private-8f8d7c-elde2e.apiary-mock.com/People/${userId}/dutyStatusChange?access_token=${token}`),
+    // axios.get(`${apiPath}/People/${userId}/dutyStatusChange?access_token=${token}`),
   },
   vehicles: {
     getVehicles: () =>
