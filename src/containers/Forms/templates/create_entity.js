b8a76047fed4c8c913eccac4c70aa7630242b5b8
Andrea Roizman <amroizman@uc.cl>
2018-05-26 19:02:22 -0400

    Added general and vehicles validations

--- src/containers/Forms/templates/create_entity.js
+++ src/containers/Forms/templates/create_entity.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button, Form, Input, Table } from 'reactstrap';
+import { Button, Form, Input, Alert } from 'reactstrap';
 import TemplateCSV from '../templates/template_csv';
 import '../../../assets/styles/forms.css';
 import api from '../../../services/api';
-import Alert from '../../Alert/Alert';
+import Alert2 from '../../Alert/Alert';
 import Loader from '../../../components/Loader/Loader';
 
 class SimpleReactFileUpload extends React.Component {
@@ -51,6 +51,7 @@ class SimpleReactFileUpload extends React.Component {
   }
 
   fileUpload(file) {
+    let { type } = this.props;
     const formData = new FormData();
     formData.append('file', file);
     const config = {
@@ -60,9 +61,18 @@ class SimpleReactFileUpload extends React.Component {
     };
 
     if (this.props.type === 'drivers') {
-      this.props.type = 'people';
+      type = 'people';
     }
-    return api.file.csvFileUpload(formData, config, this.props.token, this.props.motorCarrierId, this.props.type);
+    return api.file.csvFileUpload(formData, config, this.props.token, this.props.motorCarrierId, type);
+  }
+
+  sleep(milliseconds) {
+    const start = new Date().getTime();
+    for (let i = 0; i < 1e7; i += 1) {
+      if ((new Date().getTime() - start) > milliseconds) {
+        break;
+      }
+    }
   }
 
 
@@ -82,6 +92,10 @@ class SimpleReactFileUpload extends React.Component {
     }
     for (let i = 1; i < arr.length; i += 1) {
       const data = arr[i].split(',');
+      if (data.length !== headers.length) {
+        this.setState({ ...this.state, loading: false });
+        this.setState({ type: 'danger', message: 'Your file was not valid. Please try again later.' });
+      }
       const obj = {};
       for (let j = 0; j < data.length; j += 1) {
         obj[headers[j].trim()] = data[j].trim();
@@ -90,8 +104,6 @@ class SimpleReactFileUpload extends React.Component {
     }
     JSON.stringify(drivers);
 
-
-    /* check isValid */
     this.checkValid(drivers);
 
     if (this.state.isValid === true && this.state.loading === true) {
@@ -99,7 +111,8 @@ class SimpleReactFileUpload extends React.Component {
       this.fileUpload(this.state.file).then((response) => {
         console.log(response.data);
         console.log(response.status);
-        if (response.status === 204) {
+        if (response.status === 200) {
+          this.sleep(1000);
           this.getUploadStatus().then((res) => {
             console.log('UPLOAD STATUS');
             console.log(res);
@@ -130,53 +143,68 @@ class SimpleReactFileUpload extends React.Component {
     }
   }
 
-  checkValid(drivers) {
-    console.log(drivers);
-    for (let i = 0; i < drivers.length; i += 1) {
-      if (drivers[i].first_name.length < 2 && drivers[i].first_name.length > 30) {
-        this.setState({ isValid: false });
-        console.log('firstname');
-        return;
-      } else if (drivers[i].last_name.length < 2 && drivers[i].last_name.length > 30) {
-        this.setState({ isValid: false });
-        console.log('lastname');
-        return;
-      } else if (drivers[i].email.length < 2 && drivers[i].email.length > 30) {
-        this.setState({ isValid: false });
-        console.log('email');
-        return;
-      } else if (drivers[i].licenses_issuing_state.length === 0) {
-        this.setState({ isValid: false });
-        console.log('license');
-        return;
-      } else if (drivers[i].driver_license_number.length === 0) {
-        this.setState({ isValid: false });
-        console.log('driverlicensenumber');
-        return;
-      } else if (drivers[i].move_yards_use.length !== 1) {
-        this.setState({ isValid: false });
-        console.log('move_yards');
-        return;
-      } else if (drivers[i].default_use.length !== 1) {
-        this.setState({ isValid: false });
-        console.log('defaultuse');
-        return;
-      } else if (drivers[i].personal_use.length !== 1) {
-        this.setState({ isValid: false });
-        console.log('personaluse');
-        return;
-      } else if (drivers[i].exempt_driver_configuration.length === 0) {
-        this.setState({ isValid: false });
-        console.log('driverconfig');
-        return;
-      } else if (drivers[i].time_zone_offset_utc.length === 0) {
-        this.setState({ isValid: false });
-        console.log('time');
-        return;
-      } else if (drivers[i].starting_time_24_hour_period.length === 0) {
-        this.setState({ isValid: false });
-        console.log('24period');
-        return;
+  checkValid(data) {
+    if (this.props.type === 'drivers') {
+      console.log(data);
+      for (let i = 0; i < data.length; i += 1) {
+        if (data[i].first_name.length < 2 && data[i].first_name.length > 30) {
+          this.setState({ isValid: false });
+          console.log('firstname');
+          return;
+        } else if (data[i].last_name.length < 2 && data[i].last_name.length > 30) {
+          this.setState({ isValid: false });
+          console.log('lastname');
+          return;
+        } else if (data[i].email.length < 2 && data[i].email.length > 30) {
+          this.setState({ isValid: false });
+          console.log('email');
+          return;
+        } else if (data[i].licenses_issuing_state.length === 0) {
+          this.setState({ isValid: false });
+          console.log('license');
+          return;
+        } else if (data[i].driver_license_number.length === 0) {
+          this.setState({ isValid: false });
+          console.log('driverlicensenumber');
+          return;
+        } else if (data[i].move_yards_use.length !== 1) {
+          this.setState({ isValid: false });
+          console.log('move_yards');
+          return;
+        } else if (data[i].default_use.length !== 1) {
+          this.setState({ isValid: false });
+          console.log('defaultuse');
+          return;
+        } else if (data[i].personal_use.length !== 1) {
+          this.setState({ isValid: false });
+          console.log('personaluse');
+          return;
+        } else if (data[i].exempt_driver_configuration.length === 0) {
+          this.setState({ isValid: false });
+          console.log('driverconfig');
+          return;
+        } else if (data[i].time_zone_offset_utc.length === 0) {
+          this.setState({ isValid: false });
+          console.log('time');
+          return;
+        } else if (data[i].starting_time_24_hour_period.length === 0) {
+          this.setState({ isValid: false });
+          console.log('24period');
+          return;
+        }
+        this.setState({ isValid: true });
+      }
+    } else {
+      // Vehicles validation
+      console.log(data);
+      for (let i = 0; i < data.length; i += 1) {
+        if ((data[i].vin.length > 18 || data[i].vin.length < 17)) {
+          this.setState({ idValid: false });
+          return;
+        } else if (data[i].vin.length === 18 && String(data[i].vin)[0] !== '-') {
+          this.setState({ idValid: false });
+          return;
+        }
       }
       this.setState({ isValid: true });
     }
@@ -187,9 +215,9 @@ class SimpleReactFileUpload extends React.Component {
     let alert;
     if (this.state.type && this.state.message) {
       if (this.state.type === 'success') {
-        alert = (<Alert alertType="SUCCESS" message={this.state.message} />);
+        alert = (<Alert2 alertType="SUCCESS" message={this.state.message} />);
       } else if (this.state.type === 'danger') {
-        alert = (<Alert alertType="FAIL" message={this.state.message} />);
+        alert = (<Alert2 alertType="FAIL" message={this.state.message} />);
       }
     }
 
@@ -198,11 +226,11 @@ class SimpleReactFileUpload extends React.Component {
         <div>{ alert }</div>
         <div className="aligner">
           { this.state.errors &&
-            <Table>
-              <tbody>
-                {this.state.errors.map(error => (<tr>{ error.message }</tr>))}
-              </tbody>
-            </Table> }
+            <Alert color="danger">
+              <ul>
+                {this.state.errors.map(error => (<li key={error.id}>{ error.message }</li>))}
+              </ul>
+            </Alert>}
           <div className="aligner-item"><h1>Create multiple {this.props.type} through a csv file</h1></div>
           <div className="aligner-item"><p>The template below has the structure the csv file must have. You can download it, fill it and then upload it. That simple!</p></div>
           <div className="aligner-item"><TemplateCSV type={this.props.type} /></div>
