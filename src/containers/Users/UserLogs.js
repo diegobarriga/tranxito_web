cff696b092b0044356ca2853fd881bc6d1bacd5c
Florencia Barrios <fbarrios1@uc.cl>
2018-06-14 16:54:29 -0400

    Added filter to logs

--- src/containers/Users/UserLogs.js
+++ src/containers/Users/UserLogs.js
@@ -26,12 +26,14 @@ class UserLogs extends React.Component {
     super(props);
     this.state = {
       logs: null,
+      filterLogs: null,
       selectedSortId: null,
       selectedTypeSort: null,
       loading: true,
     };
     this.sortByColumnUp = this.sortByColumnUp.bind(this);
     this.sortByColumnDown = this.sortByColumnDown.bind(this);
+    this.filterByEvent = this.filterByEvent.bind(this);
   }
 
   componentDidMount() {
@@ -47,7 +49,7 @@ class UserLogs extends React.Component {
           const logs = response.data;
           console.log('paso ok');
           logs.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-          this.setState({ logs, loading: false });
+          this.setState({ logs, filterLogs: logs, loading: false });
         } catch (error) {
           console.log('errror');
           this.setState({ loading: false });
@@ -63,21 +65,31 @@ class UserLogs extends React.Component {
     const { logs } = this.state;
 
     logs.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-    this.setState({ logs, selectedSortId: '1', selectedTypeSort: '0' });
+    this.setState({ filterLogs: logs, selectedSortId: '1', selectedTypeSort: '0' });
   }
 
   sortByColumnUp() {
     const { logs } = this.state;
 
     logs.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-    this.setState({ logs, selectedSortId: '1', selectedTypeSort: '1' });
+    this.setState({ filterLogs: logs, selectedSortId: '1', selectedTypeSort: '1' });
   }
 
-  filterByEvent() {
+  filterByEvent(event) {
     const { logs } = this.state;
 
-    logs.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-    this.setState({ logs, selectedSortId: '1', selectedTypeSort: '1' });
+    let filteredLogs;
+    console.log(event.target.value);
+    console.log(logs);
+    if (event.target.value === -1) {
+      filteredLogs = logs;
+    } else {
+      filteredLogs = logs.filter(log => ((
+        String(log.type) === String(event.target.value)
+      )));
+    }
+
+    this.setState({ filterLogs: filteredLogs, selectedSortId: '0', selectedTypeSort: '0' });
   }
 
   render() {
@@ -125,10 +137,10 @@ class UserLogs extends React.Component {
                     />
                   </button> */}
 
-                  <select id="bender" value={this.state.filterOption} onChange={() => this.filterByEvent()}>
-                    <option key={-1} value={-1}>Event</option>
+                  <select id="bender" value={this.state.filterOption} onChange={this.filterByEvent}>
+                    <option key={-1} value={-1}>Events</option>
                     {Object.keys(EVENT_TYPES).map(key => (
-                      <option key={key} value={EVENT_TYPES[key]}>{EVENT_TYPES[key]}</option>
+                      <option key={key} value={key}>{EVENT_TYPES[key]}</option>
                    ))}
                   </select>
                 </th>
@@ -142,7 +154,7 @@ class UserLogs extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {this.state.logs.map(event => (
+              {this.state.filterLogs.map(event => (
                 <tr key={event.id}>
                   <td>{event.type === 1 &&
                     <Badge className={`event${event.code}`} style={styles.badge}>
